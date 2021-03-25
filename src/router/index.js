aaada499b43ceebdc9674beafc8d3638d33746a8
SaturdayUp <1551143601@qq.com>
2021-03-25 14:06:56 +0800

    首次提交

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from "@/views/home/Home";
+import Category from "@/views/category/Category";
+import Cart from "@/views/cart/Cart";
+import Profile from "@/views/profile/Profile";
+
+Vue.use(VueRouter)
+
+const routes=[
+  {
+    path:'',
+    redirect:'/home'
+  },
+  {
+    path: '/home',
+    component:Home
+  },
+  {
+    path: '/Category',
+    component: Category
+  },
+  {
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/profile',
+    component: Profile
+  }
+]
+const router=new VueRouter({
+  routes,
+  mode:'history'
+})
+export default router
